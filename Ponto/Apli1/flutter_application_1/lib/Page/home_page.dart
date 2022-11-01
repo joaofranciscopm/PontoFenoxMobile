4ddb93f9f86703a04ca0512438a5b1e7a0fe9151
joaofranciscopm <78879881+joaofranciscopm@users.noreply.github.com>
2022-11-01 15:23:15 -0300

    Update home_page.dart

---
+++ Ponto/Apli1/flutter_application_1/lib/Page/home_page.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple[000],
+    );
+  }
+}
